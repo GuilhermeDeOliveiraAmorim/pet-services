8f35cdcd1d894051aa8092cd7f1cd7522e878022
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2026-01-15 21:45:52 -0300

    refactor: implementa caso de uso para registro de usuário com validação e criação

---
+++ pet-services-api/internal/usecases/register_user.go
@@ -0,0 +1,111 @@
+package usecases
+
+import (
+	"context"
+	"pet-services-api/internal/entities"
+	"pet-services-api/internal/exceptions"
+	"pet-services-api/internal/logging"
+)
+
+type RegisterUserInput struct {
+	Name     string           `json:"name"`
+	UserType string           `json:"user_type"`
+	Login    entities.Login   `json:"login"`
+	Phone    entities.Phone   `json:"phone"`
+	Address  entities.Address `json:"address"`
+}
+
+type RegisterUserOutput struct {
+	Message string `json:"message,omitempty"`
+	Detail  string `json:"detail,omitempty"`
+}
+
+type RegisterUserUseCase struct {
+	userRepository entities.UserRepository
+}
+
+func NewRegisterUserUseCase(userRepository entities.UserRepository) *RegisterUserUseCase {
+	return &RegisterUserUseCase{
+		userRepository: userRepository,
+	}
+}
+
+func (uc *RegisterUserUseCase) Execute(ctx context.Context, input RegisterUserInput) (*RegisterUserOutput, []exceptions.ProblemDetails) {
+	const from = "RegisterUserUseCase.Execute"
+
+	exists, err := uc.userRepository.ExistsByEmail(input.Login.Email)
+	if err != nil {
+		return nil, logging.InternalServerError(ctx, from, "Erro ao verificar email", err)
+	}
+
+	if exists {
+		return nil, []exceptions.ProblemDetails{
+			exceptions.NewProblemDetails(exceptions.Conflict, exceptions.ErrorMessage{
+				Title:  "Email já cadastrado",
+				Detail: "O email informado já está em uso por outro usuário",
+			}),
+		}
+	}
+
+	problems := []exceptions.ProblemDetails{}
+
+	login, problems := entities.NewLogin(input.Login.Email, input.Login.Password)
+	if len(problems) > 0 {
+		problems = append(problems, problems...)
+	}
+
+	if err := login.EncryptPassword(); err != nil {
+		return nil, logging.InternalServerError(ctx, from, "Erro ao criptografar senha", err)
+	}
+
+	phone, problems := entities.NewPhone(input.Phone.CountryCode, input.Phone.AreaCode, input.Phone.Number)
+	if len(problems) > 0 {
+		problems = append(problems, problems...)
+	}
+
+	location, problems := entities.NewLocation(input.Address.Location.Latitude, input.Address.Location.Longitude)
+	if len(problems) > 0 {
+		problems = append(problems, problems...)
+	}
+
+	address, problems := entities.NewAddress(
+		input.Address.Street,
+		input.Address.Number,
+		input.Address.Neighborhood,
+		input.Address.City,
+		input.Address.ZipCode,
+		input.Address.State,
+		input.Address.Country,
+		input.Address.Complement,
+		*location,
+	)
+
+	if len(problems) > 0 {
+		problems = append(problems, problems...)
+	}
+
+	user, problems := entities.NewUser(
+		input.Name,
+		input.UserType,
+		*login,
+		*phone,
+		*address,
+	)
+
+	if len(problems) > 0 {
+		problems = append(problems, problems...)
+	}
+
+	if len(problems) > 0 {
+		return nil, problems
+	}
+
+	if err := uc.userRepository.Create(user); err != nil {
+		return nil, logging.InternalServerError(ctx, from, "Erro ao criar usuário", err)
+	}
+
+	return &RegisterUserOutput{
+		Message: "Usuário registrado com sucesso",
+		Detail:  "O usuário foi criado e registrado no sistema com sucesso",
+	}, nil
+}
