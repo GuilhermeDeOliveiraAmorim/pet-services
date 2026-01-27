1db2241bfcca797abfe1719b21dd0ac82bfdb97f
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2026-01-27 11:20:09 -0300

    refactor: adiciona manipulador de usuários com validação de ULID

---
+++ pet-services-api/internal/handlers/handlers.go
@@ -0,0 +1,45 @@
+package handlers
+
+import (
+	"context"
+	"pet-services-api/internal/database"
+	"pet-services-api/internal/exceptions"
+	"pet-services-api/internal/factories"
+	"pet-services-api/internal/logging"
+
+	"github.com/oklog/ulid/v2"
+)
+
+type HandlerFactory struct {
+	UserHandler *UserHandler
+}
+
+func NewHandlerFactory(inputFactory database.StorageInput) *HandlerFactory {
+	userFactory := factories.NewUserFactory(inputFactory.DB)
+
+	return &HandlerFactory{
+		UserHandler: NewUserHandler(userFactory),
+	}
+}
+
+func IsValidULID(ctx context.Context, id string) bool {
+	var isValid = true
+
+	_, err := ulid.Parse(id)
+
+	if err != nil {
+		logging.NewLogger(logging.Logger{
+			Context: ctx,
+			TypeLog: logging.LoggerTypes.ERROR,
+			Layer:   logging.LoggerLayers.INTERFACE_HANDLERS,
+			Code:    exceptions.RFC500_CODE,
+			From:    "IsValidULID",
+			Message: "ID inválido: " + id,
+			Error:   err,
+		})
+
+		isValid = false
+	}
+
+	return isValid
+}
