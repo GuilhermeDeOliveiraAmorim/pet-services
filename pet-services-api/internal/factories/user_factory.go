b70e1c3c87f0b5c23b81eae60919973a7eae2b11
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2026-03-11 14:31:40 -0300

    feat(factory): remover caso de uso de atualização de email verificado e ajustar estrutura do UserFactory

--- pet-services-api/internal/factories/user_factory.go
+++ pet-services-api/internal/factories/user_factory.go
@@ -10,20 +10,19 @@ import (
 )
 
 type UserFactory struct {
-	RegisterUser        *usecases.RegisterUserUseCase
-	RegisterAdmin       *usecases.CreateAdminUseCase
-	GetProfile          *usecases.GetProfileUseCase
-	ListUsers           *usecases.ListUsersUseCase
-	UpdateUser          *usecases.UpdateUserUseCase
-	DeleteUser          *usecases.DeleteUserUseCase
-	ReactivateUser      *usecases.ReactivateUserUseCase
-	DeactivateUser      *usecases.DeactivateUserUseCase
-	GetUserByID         *usecases.GetUserByIDUseCase
-	CheckEmailExists    *usecases.CheckEmailExistsUseCase
-	CheckPhoneExists    *usecases.CheckPhoneExistsUseCase
-	UpdateEmailVerified *usecases.UpdateEmailVerifiedUseCase
-	ChangePassword      *usecases.ChangePasswordUseCase
-	AddUserPhoto        *usecases.AddUserPhotoUseCase
+	RegisterUser     *usecases.RegisterUserUseCase
+	RegisterAdmin    *usecases.CreateAdminUseCase
+	GetProfile       *usecases.GetProfileUseCase
+	ListUsers        *usecases.ListUsersUseCase
+	UpdateUser       *usecases.UpdateUserUseCase
+	DeleteUser       *usecases.DeleteUserUseCase
+	ReactivateUser   *usecases.ReactivateUserUseCase
+	DeactivateUser   *usecases.DeactivateUserUseCase
+	GetUserByID      *usecases.GetUserByIDUseCase
+	CheckEmailExists *usecases.CheckEmailExistsUseCase
+	CheckPhoneExists *usecases.CheckPhoneExistsUseCase
+	ChangePassword   *usecases.ChangePasswordUseCase
+	AddUserPhoto     *usecases.AddUserPhotoUseCase
 }
 
 func NewUserFactory(db *gorm.DB, storageService storage.ObjectStorage, logger logging.LoggerInterface) *UserFactory {
@@ -33,19 +32,18 @@ func NewUserFactory(db *gorm.DB, storageService storage.ObjectStorage, logger lo
 	photoRepo := repository_impl.NewPhotoRepository(db)
 
 	return &UserFactory{
-		RegisterUser:        usecases.NewRegisterUserUseCase(userRepo, logger),
-		RegisterAdmin:       usecases.NewCreateAdminUseCase(userRepo, logger),
-		GetProfile:          usecases.NewGetProfileUseCase(userRepo, providerRepo, storageService, logger),
-		ListUsers:           usecases.NewListUsersUseCase(userRepo, storageService, logger),
-		UpdateUser:          usecases.NewUpdateUserUseCase(userRepo, storageService, logger),
-		DeleteUser:          usecases.NewDeleteUserUseCase(userRepo, logger),
-		ReactivateUser:      usecases.NewReactivateUserUseCase(userRepo, logger),
-		DeactivateUser:      usecases.NewDeactivateUserUseCase(userRepo, tokenRepo, logger),
-		GetUserByID:         usecases.NewGetUserByIDUseCase(userRepo, storageService, logger),
-		CheckEmailExists:    usecases.NewCheckEmailExistsUseCase(userRepo, logger),
-		CheckPhoneExists:    usecases.NewCheckPhoneExistsUseCase(userRepo, logger),
-		UpdateEmailVerified: usecases.NewUpdateEmailVerifiedUseCase(userRepo, logger),
-		ChangePassword:      usecases.NewChangePasswordUseCase(userRepo, logger),
-		AddUserPhoto:        usecases.NewAddUserPhotoUseCase(userRepo, photoRepo, storageService, logger),
+		RegisterUser:     usecases.NewRegisterUserUseCase(userRepo, logger),
+		RegisterAdmin:    usecases.NewCreateAdminUseCase(userRepo, logger),
+		GetProfile:       usecases.NewGetProfileUseCase(userRepo, providerRepo, storageService, logger),
+		ListUsers:        usecases.NewListUsersUseCase(userRepo, storageService, logger),
+		UpdateUser:       usecases.NewUpdateUserUseCase(userRepo, storageService, logger),
+		DeleteUser:       usecases.NewDeleteUserUseCase(userRepo, logger),
+		ReactivateUser:   usecases.NewReactivateUserUseCase(userRepo, logger),
+		DeactivateUser:   usecases.NewDeactivateUserUseCase(userRepo, tokenRepo, logger),
+		GetUserByID:      usecases.NewGetUserByIDUseCase(userRepo, storageService, logger),
+		CheckEmailExists: usecases.NewCheckEmailExistsUseCase(userRepo, logger),
+		CheckPhoneExists: usecases.NewCheckPhoneExistsUseCase(userRepo, logger),
+		ChangePassword:   usecases.NewChangePasswordUseCase(userRepo, logger),
+		AddUserPhoto:     usecases.NewAddUserPhotoUseCase(userRepo, photoRepo, storageService, logger),
 	}
 }
